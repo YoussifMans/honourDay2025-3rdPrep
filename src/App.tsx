fd7134756bd31ecf4d1ac7b6fccca65f9c5680d8
YoussifMans <youssif.ahmed.mansour.0615@gmail.com>
2025-06-18 15:03:10 +0200

    Added introductory paragraph and contents.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -8,6 +9,20 @@ function App() {
                 <h3>2025 - 3rd Prep</h3>
                 <img src="http://placehold.co/256" alt="Honour Day Logo" />
             </div>
+
+            <p>
+                ELS is a panel of greatness because it supplies its students
+                with the skills and requirements that are needed to be one of
+                the greats of the 21st Century and to be respected and loved by
+                the people. It teaches them valuable skills and traits such as:
+            </p>
+            <div className="marquee"></div>
+            <p>
+                We at ELS call them “The 6 Keys of Success.” Equipped with these
+                valuable and important skills, the students are ready to go out
+                and become successful in the 21st Century and become one of the
+                greats.
+            </p>
         </>
     );
 }
